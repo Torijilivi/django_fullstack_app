d6abed5b63dd1d72cdbfba317052caf23db17c1f
mtori <mtorijaoliva@gmail.com>
2025-05-25 21:25:47 +0200

    create, list, delete crud operations created for Note with jwt authentication

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -4,9 +4,17 @@ from api.views import CreateUserView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
+    #View for admin site
     path('admin/', admin.site.urls),
+    #View for user registration
     path('api/users/register/', CreateUserView.as_view(), name="register"),
+    #View for creating access token and refresh token
     path("api/token/", TokenObtainPairView.as_view(), name="get_token"),
+    #View for refreshing access token
     path("api/token/refresh/", TokenRefreshView.as_view(), name="refresh"),
-    path("api-auth/", include("rest_framework.urls"))
+    path("api-auth/", include("rest_framework.urls")),
+    
+    #Path for Notes RestAPI
+    path("api/", include("api.urls")),
+    
 ]

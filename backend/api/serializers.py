0ba52d23b0753e358921e4220d47b8dd2d5e0edb
mtori <mtorijaoliva@gmail.com>
2025-05-28 23:35:24 +0200

    full app ready for deployment

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -15,5 +15,5 @@ class UserSerializer(serializers.ModelSerializer):
 class NoteSerializer(serializers.ModelSerializer):
     class Meta:
         model = Note
-        fields = ["title", "content", "created_at", "author"]
+        fields = ["id","title", "content", "created_at", "author"]
         extra_kwargs = {"author": {"read_only": True}}
